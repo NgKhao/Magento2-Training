965344e655d560083c1474f2d87d4d5705520e67
khao <bsbnk141@gmail.com>
2026-01-12 09:56:44 +0700

    create data grid by UiComponenet

---
+++ app/code/Magenest/Movie/Model/ResourceModel/Movie/Grid/Collection.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Magenest\Movie\Model\ResourceModel\Movie\Grid;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+
+class Collection extends SearchResult{
+
+}
